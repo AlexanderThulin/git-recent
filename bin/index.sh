779c3e2b768ac7326802898f6349bb30cff28fbf
Alexander Thulin <alexander.t@bokio.se>
2021-11-07 20:45:04 +0100

    feat: add list mode and change default to select mode

--- bin/index.sh
+++ bin/index.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 LIMIT=10
+LIST=false
 
-while test $# -gt 0; do
+while [ $# -gt 0 ]; do
   case "$1" in
     -h|--help)
+      echo "-c NUMBER, --checkout=NUMBER   specify the number of the branch you want to checkout"
       echo "-h, --help                     show available options"
-      echo "-c NUMBER, --checkout=NUMBER   specify the number of the branch you want to check out"
-			echo "-l NUMBER, --limit=NUMBER      specify the maximum number of branches you want to list"
+      echo "-l, --list                     show list of branches, without selecting one to checkout"
+			echo "-n NUMBER, --number=NUMBER     specify the maximum number of branches you want to list"
       exit 0
       ;;
     -c)
       shift
-      if test $# -gt 0; then
+      if [ $# -gt 0 ]; then
         export BRANCH_NR=$1
       else
         echo "no branch number specified"
         exit 1
       fi
-      shift
       ;;
     --checkout*)
       CHECKOUT_VAL=$(echo $1 | sed -e 's/^[^=]*=//g')
@@ -28,19 +29,17 @@ while test $# -gt 0; do
       	echo "no branch number specified"
         exit 1
 			fi
-      shift
       ;;
-		-l)
+		-n)
       shift
-      if test $# -gt 0; then
+      if [ $# -gt 0 ]; then
         LIMIT=$1
       else
         echo "no limit specified"
         exit 1
       fi
-      shift
       ;;
-    --limit*)
+    --number*)
       LIMIT_VAL=$(echo $1 | sed -e 's/^[^=]*=//g')
       if [ -n "$LIMIT_VAL" ]; then
         LIMIT="$LIMIT_VAL"
@@ -48,25 +47,57 @@ while test $# -gt 0; do
       	echo "no limit specified"
         exit 1
 			fi
-      shift
+      ;;
+    -l|--list)
+      LIST=true
       ;;
     *)
-      break
+      echo "-c NUMBER, --checkout=NUMBER   specify the number of the branch you want to checkout"
+      echo "-h, --help                     show available options"
+      echo "-l, --list                     show list of branches, without selecting one to checkout"
+			echo "-n NUMBER, --number=NUMBER     specify the maximum number of branches you want to list"
+      exit 0
       ;;
   esac
+  shift
 done
 
-RECENT=$( \
-  git reflog \
-  | egrep -io 'moving from ([^[:space:]]+)' \
-  | awk ' !visited[$0]++' \
-  | awk '{ print NR" -", $3 }' \
-  | head -n $LIMIT
-)
+if [ "$LIST" = false ] && [ -z $BRANCH_NR ]; then
+  RECENT=(
+    $( \
+      git reflog \
+      | egrep -io 'moving from ([^[:space:]]+)' \
+      | awk ' !visited[$0]++' \
+      | awk '{ print $3 }' \
+      | head -n $LIMIT
+    )
+  )
 
-if [ -n "$BRANCH_NR" ]; then
-  BRANCH=$(grep "^$BRANCH_NR\s" <<< "$RECENT" | awk '{ print $2 }')
-  git checkout "$BRANCH"
+  PS3="Select branch to checkout: "
+  
+  select OPTION in "${RECENT[@]}"; do
+    if [ -n "$OPTION" ]; then
+      git checkout "$OPTION"
+      break
+    else
+      echo "Invalid option";
+    fi
+  done
+
 else
-  echo "$RECENT"
+  RECENT=$( \
+    git reflog \
+    | egrep -io 'moving from ([^[:space:]]+)' \
+    | awk ' !visited[$0]++' \
+    | awk '{ print NR, "-", $3 }' \
+    | head -n $LIMIT
+  )
+
+  if [ -n "$BRANCH_NR" ]; then
+    BRANCH=$(grep "^$BRANCH_NR\s" <<< "$RECENT" | awk '{ print $3 }')
+    git checkout "$BRANCH"
+
+  else
+    echo "$RECENT"
+  fi
 fi
